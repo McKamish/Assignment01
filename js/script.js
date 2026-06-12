541474475ac37d291a442f60c78f0c91b689049f
Cory Jameson <corymjameson@gmail.com>
2026-06-12 16:01:05 -0700

    Finished assignment

---
+++ js/script.js
@@ -0,0 +1,112 @@
+//STEP 1
+//let someMonth;
+//function theMonth();
+//let currentMonth;
+//let summerMonth;
+//let myLibraryFunction;
+
+//STEP 2
+//1
+//'jabberwocky'
+//true
+//null
+
+//STEP 3
+//let total = 8 + 4;
+//let answer = prompt('What is the answer?');
+
+//STEP 4
+//let firstName
+//let lastName
+//let strAddress
+//let strCity
+//let strState
+//let zipCode
+//let yourAge
+//let referralSource
+//let mayWeContactYou
+
+//STEP 5
+
+//let yourAge = prompt('What is your age?')
+//let referralSource = 'City Watch, LLC'
+//let mayWeContactYou = confirm('May we contact you? Select OK if yes')
+
+//STEP 6
+//let funVariable = 1 + ' day at a time'
+//console.log(funVariable)
+
+//STEP 7
+//let booThang = true + ' string'
+//console.log(booThang)
+//
+//let numbBoo = 5 + true
+//console.log(numbBoo)
+
+//STEP 8
+//It is not Valid. You must escape the single quote with a backslash:
+//let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+//console.log(someString)
+
+//STEP 9
+
+//let notExistent = null
+//console.log(notExistent = null)
+//let notANumb = 'notanumb' / 5
+//console.log(notANumb)
+
+//STEP 10
+
+//typeof 'Tweety Bird'
+//typeof 5
+//typeof false
+//typeof console
+//typeof log
+
+//STEP 11
+//alert('Hello ' + 'Cory Jameson' + ', welcome to the JavaScript class!')
+
+//STEP 12
+
+//let name = 'Cory Jameson'
+//alert('Hello ' + name + ', welcome to the JavaScript class!')
+
+//STEP 13
+
+//let course = 'JavaScript'
+//alert('Hello ' + name + ', welcome to the ' + course + ' class!')
+
+//STEP 14
+//let name = 'Cory Jameson'
+//let course = 'JavaScript'
+//alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+//STEP 15
+//let course = 'JavaScript'
+//let name = prompt('What is your name?')
+//alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+//STEP 16
+//let name = prompt('What is your name?')
+//let course = prompt('What class are you taking?')
+//alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+//STEP 17
+//let x = 10
+//let y = 20
+//console.log(x + y)
+
+//STEP 18
+//let x = 20
+//x += 20
+//console.log(x)
+
+//STEP 19
+//let x = 20
+//x *= 5
+//console.log(x)
+
+//STEP 20
+//let x = 20 % 3
+//x /= 1
+//console.log(x)
